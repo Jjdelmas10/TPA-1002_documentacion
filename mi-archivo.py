ef994b910a582c0b9fc388be710cea9b7d47e323
Jjdelmas10 <juanjomas10@hotmail.com>
2026-09-14 20:27:23 -0600

    Agregar mi-archivo.py

---
+++ mi-archivo.py
@@ -0,0 +1,17 @@
+def saludar(nombre):
+    print(f"Hola, {nombre}. Bienvenido al ejercicio de Git y GitHub!")
+
+
+def sumar(a, b):
+    resultado = a + b
+    print(f"La suma de {a} + {b} es: {resultado}")
+
+
+def main():
+    print("Hola, mundo desde Python")
+    saludar("Francisco")
+    sumar(5, 3)
+
+
+if __name__ == "__main__":
+    main()
